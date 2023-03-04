be860d1997ca1e77cdf9a4fdfc03e875d8a2049b
vi-lcandido <vitoria.lcandido@gmail.com>
2023-03-04 10:16:45 -0300

    feat: weather request

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
+import axios from "axios";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const baseUrlWeather = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const [cityName, setCityName] = useState("");
+  const [data, setData] = useState({});
+  
+  const key = "bbf164b1840641ac124799d6d35c5289"
+  const baseUrlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
+
+  const searchCity = (event) => {
+    if (event.key === "Enter") {
+      axios.get(baseUrlWeather).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
 
   return (
     <div className="app">
       <div className="container">
+        {/* <div> */}
         <div className="header">
-          <input placeholder="Digite uma cidade"></input>
+          <input 
+          placeholder="Digite uma cidade"
+          value={cityName}
+          onChange={event => setCityName(event.target.value)}
+          onKeyDown={searchCity}
+          type="text"
+          ></input>
         </div>
-        <div className="weather-container">
+        <div className="inside-container">
           <h1>Clima</h1>
           <h3>Manaus</h3>
           <div className="description">
@@ -22,10 +42,11 @@ function App() {
             <p>50ºC</p>
           </div>
         </div>
+        {/* </div> */}
         <div>
-          <div className="pokemon-container">
+          <div className="inside-container">
+            <h1>Pokemón</h1>
             <div className="description">
-              <h1>Pokemón</h1>
               <p>Tipo do pokemon</p>
               <p>Nome pokemon</p>
             </div>
