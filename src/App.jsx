b5a6370c3908b19d36765c51475b908ac7e740ce
vi-lcandido <vitoria.lcandido@gmail.com>
2023-03-05 16:49:21 -0300

    refactor: removing unnecessary functions from app

--- src/App.jsx
+++ src/App.jsx
@@ -2,92 +2,21 @@ import axios from "axios";
 import { useState } from "react";
 import { searchPokemonFromCity, searchPokemon } from "./services/respository";
 
-// import './App.css'
-
 function App() {
-  // const [cityName, setCityName] = useState("");
   const [dataWeather, setDataWeather] = useState({});
   const [pokemonType, setPokemonType] = useState("");
   const [pokemonName, setPokemonName] = useState("");
   const [temp, setTemp] = useState("");
 
-  const key = "bbf164b1840641ac124799d6d35c5289";
-  // const urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
-
-  const urlPokemon = "https://pokeapi.co/api/v2/type/";
-
   const searchCity = async (event) => {
     if (event.key === "Enter") {
-      console.log(event.target.value);
-      let { dataClima, temp, tipoPokemon, nomePokemon } = await searchPokemonFromCity(
-        event.target.value
-      );
+      let { dataClima, temp, tipoPokemon, nomePokemon } =
+        await searchPokemonFromCity(event.target.value);
       setTemp(temp);
       setDataWeather(dataClima);
       setPokemonType(tipoPokemon);
       setPokemonName(nomePokemon);
-      // let response = await axios.get(urlWeather);
-      // let resultWeather = response.data;
-      // setDataWeather(resultWeather);
-      // let tempCelcius = celsiusTemp(resultWeather);
-      // // console.log(tempCelcius);
-      // if (resultWeather.weather[0].main === "Rain") {
-      //   console.log(await searchPokemon("electric"));
-      // } else if (tempCelcius < 5) {
-      //   console.log(await searchPokemon("ice"));
-      // } else if (tempCelcius < 10) {
-      //   console.log(await searchPokemon("water"));
-      // } else if (tempCelcius >= 12 && tempCelcius < 15) {
-      //   console.log(await searchPokemon("grass"));
-      // } else if (tempCelcius >= 15 && tempCelcius < 21) {
-      //   console.log(await searchPokemon("ground"));
-      // } else if (tempCelcius >= 23 && tempCelcius < 27) {
-      //   console.log(await searchPokemon("bug"));
-      // } else if (tempCelcius >= 27 && tempCelcius <= 33) {
-      //   console.log(await searchPokemon("rock"));
-      // } else if (tempCelcius > 33) {
-      //   console.log(await searchPokemon("fire"));
-      // } else {
-      //   console.log(await searchPokemon("normal"));
-      // }
     }
-  };
-
-  const searchPokemon = async (pokemonType) => {
-    let response = await axios.get(`${urlPokemon}${pokemonType}`);
-    let resultPokemon = response.data;
-
-    const randomPokemon = Math.floor(
-      Math.random() * (resultPokemon.pokemon.length - 1)
-    );
-    setPokemonType(pokemonType);
-    setPokemonName(resultPokemon.pokemon[randomPokemon].pokemon.name);
-    let resultPokemonList = resultPokemon.pokemon;
-
-    if (
-      localStorage.getItem("nome") ===
-      resultPokemonList[randomPokemon].pokemon.name
-    ) {
-      if (randomPokemon == resultPokemonList.length - 1) {
-        localStorage.setItem(
-          "nome",
-          resultPokemonList[randomPokemon - 1].pokemon.name
-        );
-      } else {
-        localStorage.setItem(
-          "nome",
-          resultPokemonList[randomPokemon + 1].pokemon.name
-        );
-      }
-    }
-
-    return resultPokemonList[randomPokemon].pokemon.name;
-  };
-
-  const celsiusTemp = (weather) => {
-    let tempCelcius = Math.round(parseFloat(weather.main.temp) - 273.15);
-
-    return tempCelcius;
   };
 
   return (
@@ -97,8 +26,6 @@ function App() {
           <div className="search">
             <input
               placeholder="Digite uma cidade"
-              // value={cityName}
-              // onChange={(event) => setCityName(event.target.value)}
               onKeyDown={searchCity}
               type="text"
             ></input>
