bbd4107f05065dd35d1913e5678609d9ccc1ca45
vi-lcandido <vitoria.lcandido@gmail.com>
2023-03-05 20:02:08 -0300

    del: log unnecessary

--- src/services/respository.js
+++ src/services/respository.js
@@ -13,7 +13,7 @@ export const searchPokemonFromCity = async (cityName) => {
 
     let { responsePokemonType, responsePokemonName } =
       await getPokemonFromWeather(resultWeather.weather[0].main, tempCelcius);
-console.log(responsePokemonName.charAt(0).toUpperCase() + responsePokemonType.substring(1));
+
     return {
       dataClima: resultWeather,
       temp: tempCelcius,
